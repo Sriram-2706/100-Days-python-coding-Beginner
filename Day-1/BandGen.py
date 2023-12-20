cc1bf2c7456417013031a5ffb009a842d7a1dc04
Sriram-2706 <nsri.1340@gmail.com>
2023-12-20 21:30:38 +0530

    Create BandGen.py

---
+++ Day-1/BandGen.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator ")
+k=input("What's the name of the city you spent your  childhood?")
+m=input("What's Your Favourite charcter of all Time?")
+print("Your Band Name could be ",k,m)
